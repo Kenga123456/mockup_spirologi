854656767a4586f66297d0b95fd654badf4ce4ae
Kenga123456 <107131339+Kenga123456@users.noreply.github.com>
2025-10-30 11:02:33 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+fetch("bilder.json")
+  .then((res) => res.json())
+  .then((images) => {
+    const imgContainer = document.getElementById("images-container");
+
+    images.forEach((src) => {
+      const child = document.createElement("div");
+      const img = document.createElement("img");
+
+      img.src = `https://images.weserv.nl/?url=${encodeURIComponent( //for å fikse problem med at browser ikke liker http elns :(
+        src.replace(/^https?:\/\//, "")
+      )}`;
+
+      img.style.width = "100px";
+
+      child.appendChild(img);
+      imgContainer.appendChild(child);
+    });
+  });
